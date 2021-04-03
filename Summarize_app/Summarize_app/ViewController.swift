9a9db866c078865b0d867a44ef0b06d561423bbd
ahamedarif99 <ahamed.arif@stonybrook.edu>
2021-04-03 19:29:38 -0400

    initial commit of the project

---
+++ Summarize_app/Summarize_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Summarize_app
+//
+//  Created by Ahamed Arif on 4/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
