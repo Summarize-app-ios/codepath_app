4ed9bc3fb0787553429132ebd586b3d3de6a8687
ahamedarif99 <ahamed.arif@stonybrook.edu>
2021-04-22 15:25:52 -0400

    added the trending and personalized blank view controllers

---
+++ Summarize_app/Summarize_app/TrendingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TrendingViewController.swift
+//  Summarize_app
+//
+//  Created by Ahamed Arif on 4/22/21.
+//
+
+import UIKit
+
+class TrendingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
