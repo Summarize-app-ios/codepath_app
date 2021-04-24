4c1d906c3c14fde2d4502832bb83577e83f99dff
1045043654 <andrewlin3247@gmail.com>
2021-04-24 14:56:04 -0400

    1

--- Summarize_app/Summarize_app/ViewController.swift
+++ Summarize_app/Summarize_app/ViewController.swift
@@ -10,31 +10,31 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var userNameField: UITextField!
-    
+
     @IBOutlet weak var passwordField: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-    
+
+
     @IBAction func signIn(_ sender: Any) {
         let username = userNameField.text!
         let password = passwordField.text!
-        
+
         self.performSegue(withIdentifier: "success", sender: nil)
-        
+
     }
-    
+
     @IBAction func signUp(_ sender: Any) {
-        
+
         let username = userNameField.text
         let password = passwordField.text
-        
+
         self.performSegue(withIdentifier: "success", sender: nil)
-    
+
     }
 
 
