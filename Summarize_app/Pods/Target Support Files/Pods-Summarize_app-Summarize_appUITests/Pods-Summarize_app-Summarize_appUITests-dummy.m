be8efff5423daa4d0ac627818ff4a8890915d8fe
ahamedarif99 <ahamed.arif@stonybrook.edu>
2021-04-24 15:21:40 -0400

    who knows

---
+++ Summarize_app/Pods/Target Support Files/Pods-Summarize_app-Summarize_appUITests/Pods-Summarize_app-Summarize_appUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Summarize_app_Summarize_appUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Summarize_app_Summarize_appUITests
+@end
---
+++ Summarize_app/Pods/Target Support Files/Pods-Summarize_appTests/Pods-Summarize_appTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Summarize_appTests : NSObject
+@end
+@implementation PodsDummy_Pods_Summarize_appTests
+@end
