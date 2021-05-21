672bd0426398060320e0b42bccbca7d33c9b71a1
ahamedarif99 <ahamed.arif@stonybrook.edu>
2021-05-21 01:25:34 -0400

    final

--- Summarize_app/Summarize_app/favViewController.swift
+++ Summarize_app/Summarize_app/favViewController.swift
@@ -15,7 +15,10 @@ class favViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func vtn(_ sender: Any) {
+        performSegue(withIdentifier: "logoutFav", sender: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
